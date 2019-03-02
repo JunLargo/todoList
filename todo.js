5314e2998b4acbf21ac1baeaef188371d4f3715c
JunLargo <rklargsjr@gmail.com>
2019-03-03 01:31:21 +0800

    added Version 6 - toggleAll method

--- todo.js
+++ todo.js
@@ -9,7 +9,6 @@ VERSION 3 -Requirements
 
 */
 
-
 // var todoList = {
 //     todos: ['first','second','third'],
 //     displayTodos: function() {
@@ -28,7 +27,6 @@ VERSION 3 -Requirements
 //         this.displayTodos();
 //     }
 // }
-
 
 /*
 
@@ -66,7 +64,6 @@ VERSION 4 Booleans -Requirements
 //     }
 // }
 
-
 /*
 
 VERSION 5 Loops of Logic -Requirements
@@ -76,43 +73,109 @@ VERSION 5 Loops of Logic -Requirements
 
 */
 
+// var todoList = {
+// 	todos: [],
+// 	displayTodos: function() {
+// 		if (this.todos.length === 0) {
+// 			console.log('Your todos is empty!');
+// 		} else {
+// 			console.log('My Todos:');
+// 			for (var i = 0; i < this.todos.length; i++) {
+// 				if (this.todos[i].completed === false) {
+// 					console.log('(x) ', this.todos[i].todoText);
+// 				} else {
+// 					console.log('( ) ', this.todos[i].todoText);
+// 				}
+// 			}
+// 		}
+// 	},
+// 	addTodo: function(todoText) {
+// 		this.todos.push({
+// 			todoText: todoText,
+// 			completed: false
+// 		});
+// 		this.displayTodos();
+// 	},
+// 	changeTodo: function(position, todoText) {
+// 		this.todos[position].todoText = todoText;
+// 		this.displayTodos();
+// 	},
+// 	deleTodo: function(position) {
+// 		this.todos.splice(position, 1);
+// 		this.displayTodos();
+// 	},
+// 	toggleCompleted: function(position) {
+// 		var todo = this.todos[position];
+// 		todo.completed = !todo.completed;
+// 		this.displayTodos();
+// 	}
+// };
+
+/*
+
+VERSION 6 Thingking In Code
+1.) toggleAll: If everything is true, make everything false
+2.) Otherwise, make everything false
+
+*/
 
 var todoList = {
-    todos: [],
-    displayTodos: function() {
-       
+	todos: [],
+	displayTodos: function() {
+		if (this.todos.length === 0) {
+			console.log('Your todos is empty!');
+		} else {
+			console.log('My Todos:');
+			for (var i = 0; i < this.todos.length; i++) {
+				if (this.todos[i].completed === false) {
+					console.log('( ) ', this.todos[i].todoText);
+				} else {
+					console.log('(x) ', this.todos[i].todoText);
+				}
+			}
+		}
+	},
+	addTodo: function(todoText) {
+		this.todos.push({
+			todoText: todoText,
+			completed: false
+		});
+		this.displayTodos();
+	},
+	changeTodo: function(position, todoText) {
+		this.todos[position].todoText = todoText;
+		this.displayTodos();
+	},
+	deleTodo: function(position) {
+		this.todos.splice(position, 1);
+		this.displayTodos();
+	},
+	toggleCompleted: function(position) {
+		var todo = this.todos[position];
+		todo.completed = !todo.completed;
+		this.displayTodos();
+	},
+	toggleAll: function() {
+		//completedTodos = totalTodos
+		var totalTodos = this.todos.length;
+		var completedTodos = 0;
 
-        if (this.todos.length === 0) {
-            console.log('Your todos is empty!');
-        } else {
-            console.log('My Todos:');
-            for (var i = 0; i < this.todos.length; i++) {
-                if (this.todos[i].completed === false) {
-                    console.log('(x) ', this.todos[i].todoText);
-                } else {
-                    console.log('( ) ', this.todos[i].todoText);
-                }
-            }
-        } 
-    },
-    addTodo: function(todoText) {
-       this.todos.push({
-           todoText: todoText,
-           completed: false
-       });
-       this.displayTodos();
-    },
-    changeTodo: function(position,todoText) {
-       this.todos[position].todoText = todoText;
-       this.displayTodos();
-    },
-    deleTodo: function(position) {
-        this.todos.splice(position, 1);
-        this.displayTodos();
-    },
-    toggleCompleted: function(position) {
-        var todo = this.todos[position];
-        todo.completed = !todo.completed;
-        this.displayTodos();
-    }
-}
+		//get total number of comppleted todos
+		for (var i = 0; i < totalTodos; i++) {
+			if (this.todos[i].completed === true) {
+				completedTodos++;
+			}
+		}
+
+		//if everything is true, make everything false
+		for (var i = 0; i < totalTodos; i++) {
+			if (completedTodos === totalTodos) {
+				this.todos[i].completed = false;
+			} else {
+				this.todos[i].completed = true;
+			}
+		}
+
+		todoList.displayTodos();
+	}
+};
