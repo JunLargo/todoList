cb3a6debb016282dfe24d1fd6108fdf63d35b023
JunLargo <rklargsjr@gmail.com>
2019-03-24 02:25:40 +0800

    replacing for loops with forEach

--- todo.js
+++ todo.js
@@ -417,19 +417,19 @@ var todoList = {
 		var totalTodos = this.todos.length;
 		var completedTodos = 0;
 
-		for (var i = 0; i < totalTodos; i++) {
-			if (this.todos[i].completed === true) {
+		this.todos.forEach(function(todo) {
+			if (todo.completed === true) {
 				completedTodos++;
 			}
-		}
+		});
 
-		for (var i = 0; i < totalTodos; i++) {
+		this.todos.forEach(function(todo) {
 			if (completedTodos === totalTodos) {
-				this.todos[i].completed = false;
+				todo.completed = false;
 			} else {
-				this.todos[i].completed = true;
+				todo.completed = true;
 			}
-		}
+		});
 	}
 };
 
@@ -454,7 +454,7 @@ var handlers = {
 		toggleCompletedPositionInput.value = '';
 		view.displayTodos();
 	},
-	deleTodo: function(position) {
+	deleteTodo: function(position) {
 		todoList.deleteTodo(position);
 		view.displayTodos();
 	},
@@ -469,9 +469,8 @@ var view = {
 		var todosUl = document.querySelector('ul');
 		todosUl.innerHTML = '';
 
-		for (var i = 0; i < todoList.todos.length; i++) {
+		todoList.todos.forEach(function(todo, position) {
 			var todoLi = document.createElement('li');
-			var todo = todoList.todos[i];
 			var todoTextWithCompletion = '';
 
 			if (todo.completed === true) {
@@ -480,14 +479,13 @@ var view = {
 				todoTextWithCompletion = '( ) ' + todo.todoText;
 			}
 
-			todoLi.id = i; //version 10.3
+			todoLi.id = position; //version 10.3
 			todoLi.textContent = todoTextWithCompletion;
 			todoLi.appendChild(this.createDeleteButton()); //version 10.2
 			todosUl.appendChild(todoLi);
-		}
+		}, this); //the this keyword refer to the object view
 	},
 	createDeleteButton: function() {
-		//version 10.1
 		var deleteButton = document.createElement('button');
 		deleteButton.textContent = 'Delete';
 		deleteButton.className = 'deleteButton';
@@ -495,13 +493,10 @@ var view = {
 	},
 	setUpEventListeners: function() {
 		var todosUl = document.querySelector('ul');
-
-		todosUl.addEventListener('click', function(event) {
-			//Get the element that was clicked on
-			var elementClicked = event.target;
-
-			if ((elementClicked.className = 'deleteButton')) {
-				handlers.deleTodo(parseInt(event.target.parentNode.id));
+		todosUl.addEventListener('click', function(e) {
+			var elementClicked = e.target;
+			if (elementClicked.className === 'deleteButton') {
+				handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
 			}
 		});
 	}
